432f2735d1fcaa8a91ff50447136a87d6feb3fef
rauld <rcruzadodelgado@safareyes.es>
2025-01-31 16:43:48 +0100

    ⚡️ implement CORS bundle and config

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
